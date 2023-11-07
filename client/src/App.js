9b7919916e0e2a29d53f6a8b42a7786ebd5569cd
Ischit <ischit131004@gmail.com>
2023-11-08 04:19:28 +0530

    path set

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
 import LandingPage from "./Components/LandingPage/LandingPage";
@@ -17,19 +17,45 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="login" />}></Route>
+
         <Route path="login" element={<Login />}></Route>
         <Route path="signup" element={<Signup />}></Route>
         <Route
           path="home"
-          element={<LandingPage showNotiList={showNotiList} setShowNotiList={setShowNotiList} />}
+          element={
+            <LandingPage
+              showNotiList={showNotiList}
+              setShowNotiList={setShowNotiList}
+            />
+          }
         ></Route>
         <Route
           path="organisation/:organisationName"
-          element={<Organisation showNotiList={showNotiList} setShowNotiList={setShowNotiList} />}
+          element={
+            <Organisation
+              showNotiList={showNotiList}
+              setShowNotiList={setShowNotiList}
+            />
+          }
         ></Route>
-        <Route path="createorganisation" element={<CreateOrganisation />}></Route>
-        <Route path="organisation/:organisationName/createchannel" element={<CreateChannel />}></Route>
-        <Route path="DMs" element={<DMs showNotiList={showNotiList} setShowNotiList={setShowNotiList} />}></Route>
+        <Route
+          path="createorganisation"
+          element={<CreateOrganisation />}
+        ></Route>
+        <Route
+          path="organisation/:organisationName/createchannel"
+          element={<CreateChannel />}
+        ></Route>
+        <Route
+          path="DMs"
+          element={
+            <DMs
+              showNotiList={showNotiList}
+              setShowNotiList={setShowNotiList}
+            />
+          }
+        ></Route>
         <Route path="user/:username" element={<ViewProfile />}></Route>
         <Route path="/updateProfile" element={<UpdateProfile />}></Route>
       </Routes>
